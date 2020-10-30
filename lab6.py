c340bc39ef3c194c4c57c9c7e8357d7c4f1560e2
rlchavez3 <72169569+rlchavez3@users.noreply.github.com>
2020-10-30 15:08:09 -0700

    Update lab6.py

--- lab6.py
+++ lab6.py
@@ -1,5 +1,12 @@
 import turtle
+t = Turtle.turtle()
+turtle.bgcolor('black')
+turtle.screensize(500)
+turtle.speed(0)
+turtle.tracer(1, 0)
 
 star_file = open('stars.txt', 'r')
 lines = star_file.readlines()
 star_file.close()
+
+turtle.exitonclick()
