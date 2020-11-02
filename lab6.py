a390557e802d25142dbac3d5c9f2a9c50195561b
rlchavez3 <72169569+rlchavez3@users.noreply.github.com>
2020-11-02 15:53:57 -0700

    Update lab6.py
    adding comments

--- lab6.py
+++ lab6.py
@@ -1,3 +1,4 @@
+# setting up turtle screen
 import turtle
 t = turtle.Turtle()
 turtle.bgcolor('black')
@@ -5,14 +6,19 @@ turtle.screensize(500)
 turtle.speed(0)
 turtle.tracer(1, 0)
 
+# getting data from stars file
 star_file = open('stars.txt', 'r')
 stars = star_file.readlines()
 star_file.close()
 
+# iterating through data and drawing
 for line in stars:
   split = line.split()
+  # x coordinate conversion
   x = float(split[0]) * 250
+  # y coordinate conversion
   y = float(split[1]) * 250
+  # magnitude conversion and limit
   m = round(10/(float(split[4]) + 2))
   if m > 10:
     m = 10
