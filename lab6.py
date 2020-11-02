51a12ea0855b5184cd26ce19d129b1ffbf87fd2f
rlchavez3 <72169569+rlchavez3@users.noreply.github.com>
2020-11-02 15:45:49 -0700

    Update lab6.py

--- lab6.py
+++ lab6.py
@@ -1,5 +1,5 @@
 import turtle
-t = Turtle.turtle()
+t = turtle.Turtle()
 turtle.bgcolor('black')
 turtle.screensize(500)
 turtle.speed(0)
@@ -11,9 +11,13 @@ star_file.close()
 
 for line in stars:
   split = line.split()
-  x = split[0] * 250
-  y = split[1] * 250
-  m = round(10/(split[4] + 2))
+  x = float(split[0]) * 250
+  y = float(split[1]) * 250
+  m = round(10/(float(split[4]) + 2))
+  if m > 10:
+    m = 10
+  elif m < 1:
+    m = 1
   t.up()
   t.color('white')
   t.goto(x, y)
