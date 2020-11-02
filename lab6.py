7f89b382b7eff044cbd21b6d705a2e3c901fc097
rlchavez3 <72169569+rlchavez3@users.noreply.github.com>
2020-11-02 15:21:24 -0700

    Update lab6.py

--- lab6.py
+++ lab6.py
@@ -6,7 +6,17 @@ turtle.speed(0)
 turtle.tracer(1, 0)
 
 star_file = open('stars.txt', 'r')
-lines = star_file.readlines()
+stars = star_file.readlines()
 star_file.close()
+
+for line in stars:
+  split = line.split()
+  x = split[0] * 250
+  y = split[1] * 250
+  m = round(10/(split[4] + 2))
+  t.up()
+  t.color('white')
+  t.goto(x, y)
+  t.dot(m)
 
 turtle.exitonclick()
